99908b3fb8fec90aaf1b0cd70a34642210bb1f43
Jorge González <scrub.mx@gmail.com>
2013-09-12 11:46:07 -0500

    html5shiv

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="cleartype" content="on">
+		<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 		<?php wp_head(); ?>
 	</head>
 
