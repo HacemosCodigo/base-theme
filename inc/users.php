ae4e54f8c50d4cadaaeb5eb7760202bf62d1ac66
Jorge González <scrub.mx@gmail.com>
2013-09-13 11:33:44 -0500

    Automagically create developer users

---
+++ inc/users.php
@@ -0,0 +1,58 @@
+<?php
+
+
+// CUSTOM USER CONFIGURATIONS /////////////////////////////////////////////////////////
+
+
+
+	$administrator = get_role('administrator');
+	add_role( 'developer', 'Developer', $administrator->capabilities );
+
+
+	remove_action( 'admin_color_scheme_picker', 'admin_color_scheme_picker' );
+
+
+	add_filter('user_contactmethods', function ( $contactmethods ) {
+		unset($contactmethods['url']);
+		unset($contactmethods['aim']);
+		unset($contactmethods['yim']);
+		unset($contactmethods['jabber']);
+		$contactmethods['twitter']  = 'Twitter';
+		$contactmethods['facebook'] = 'Facebook';
+		return $contactmethods;
+	});
+
+
+
+// CREATE DEFAULT USERS ///////////////////////////////////////////////////////////////
+
+
+
+	add_action('init', function(){
+		$users = array('cova', 'nori', 'alex', 'san', 'raul', 'john', 'york');
+		array_map('create_usuario_maquilador', $users);
+	});
+
+
+	/**
+	 * Crear un nuevo usuario
+	 * @param  string $user username
+	 */
+	function create_usuario_maquilador($user){
+		$password = wp_generate_password();
+		$user_id  = wp_create_user( $user, $password, "$user@losmaquiladores.com" );
+		if ( is_int($user_id) ){
+			set_maquilador_role( $user_id );
+			wp_new_user_notification( $user_id, $password );
+		}
+	}
+
+
+	/**
+	 * Set user role as developer (super admin)
+	 * @param int $user_id
+	 */
+	function set_maquilador_role($user_id){
+		$wp_user = get_user_by( 'id', $user_id );
+		$wp_user->set_role( 'developer' );
+	}
