9a653bde6dc4637b4216594615a4ef296bcc6915
Jorge González <scrub.mx@gmail.com>
2013-09-26 13:23:47 -0500

    validateEmail helper function

--- js/functions.js
+++ js/functions.js
@@ -6,6 +6,12 @@
 
 
 		console.log('hello from jquery');
+		
+		
+                window.validateEmail = function (email) {
+                        var regExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                        return regExp.test(email);
+                };
 
 
 	});
