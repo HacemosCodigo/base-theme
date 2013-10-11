54e75257c92ff62f7b9d71a78db143f57c47bf15
Jorge González <scrub.mx@gmail.com>
2013-10-11 14:06:17 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,11 +7,11 @@
 
 	define( 'JSPATH', get_template_directory_uri() . '/js/' );
 
-
 	define( 'CSSPATH', get_template_directory_uri() . '/css/' );
 
-
 	define( 'THEMEPATH', get_template_directory_uri() . '/' );
+	
+	define( 'SITEURL', site_url('/') );
 
 
 
