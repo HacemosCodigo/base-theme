61de7ab1477d8d6312f7ee1e420b2202baae22aa
Jorge González <scrub.mx@gmail.com>
2013-12-10 13:10:11 -0600

    Update pages.php

--- inc/pages.php
+++ inc/pages.php
@@ -8,11 +8,12 @@
 
 
 		// CONTACTO
-		/*if( ! get_page_by_path('contacto') ){
+		/*if( ! get_page_by_path('faq') ){
 			$page = array(
 				'post_author' => 1,
 				'post_status' => 'publish',
-				'post_title'  => 'Contacto',
+				'post_title'  => 'Frequently Asked Questions',
+				'post_name'   => 'faq'
 				'post_type'   => 'page'
 			);
 			wp_insert_post( $page, true );
