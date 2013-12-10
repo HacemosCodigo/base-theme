7df3ea7c55d15d37af6db55cc773bf9fada26b55
Jorge González <scrub.mx@gmail.com>
2013-12-10 13:21:35 -0600

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -5,13 +5,31 @@
 	$(function(){
 
 
-		console.log('hello from jquery');
+		console.log('hello from functions.js');
 		
 		
-                window.validateEmail = function (email) {
-                        var regExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                        return regExp.test(email);
-                };
+		/**
+         * Validación de emails
+         */
+		window.validateEmail = function (email) {
+			var regExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			return regExp.test(email);
+		};
+               
+               
+                
+        /**
+         * Regresa todos los valores de un formulario como un associative array 
+         */
+ 		window.getFormData = function (selector) {
+			var result = [],
+				data   = $(selector).serializeArray();
+
+			$.map(data, function (attr) {
+				result[attr.name] = attr.value;
+			});
+			return result;
+		}
 
 
 	});
